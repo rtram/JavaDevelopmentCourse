dbb7624de251ad06dc31fbecdc4067b1279b4093
Robin Tram <robin.tram@gmail.com>
2020-06-02 11:46:29 -0400

    Add Expressions Lesson and Practice

---
+++ KeywordsAndExpressions/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // kilometers, the equal sign operator and the values in the parens make up the Java expression
+	    // the "double" and the semi colon make this expression into a valid Java statement.
+        double kilometers = (100 * 1.609344);
+
+        int highScore = 50;
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+        // Java Expressions practice
+
+        // score = 100
+        // score > 99
+        // "You got the high score!"
+        // score = 0
+    }
+}
