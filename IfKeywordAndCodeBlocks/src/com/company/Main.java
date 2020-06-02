0f0f4db091ee4fabdf0b3799d9dd47a8664ec9dd
Robin Tram <robin.tram@gmail.com>
2020-06-02 12:22:34 -0400

    Add If Keyword and Code Blocks Lesson

---
+++ IfKeywordAndCodeBlocks/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    boolean gameOver = true;
+	    int score = 4000;
+	    int levelCompleted = 5;
+	    int bonus = 100;
+
+//	    if (score < 5000 && score > 1000) {
+//            System.out.println("Your score was 5000 but greater than 1000");
+//        } else if (score < 1000) {
+//			System.out.println("Your score was less 1000");
+//		} else {
+//			System.out.println("Got here");
+//		}
+
+		if (gameOver) {
+			int finalScore = score + (levelCompleted * bonus);
+			System.out.println("Your final score was " + finalScore);
+		}
+
+		score = 10000;
+		levelCompleted = 8;
+		bonus = 200;
+
+		if (gameOver) {
+			int finalScore = score + (levelCompleted * bonus);
+			System.out.println("Your final score was " + finalScore);
+		}
+    }
+}
