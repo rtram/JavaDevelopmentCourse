40e215aca953791691f096bacf98c4a0ff7759a2
Robin Tram <robin.tram@gmail.com>
2020-08-12 12:21:16 -0400

    Sum 3 and 5 Challenge

---
+++ Sum3And5Challenge/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int sum = 0;
+	    int count = 0;
+
+        for (int i = 1; i <= 1000; i++) {
+	        if (i % 3 == 0 && i % 5 == 0) {
+	            sum += i;
+	            count++;
+                System.out.println(i);
+                if(count == 5) {
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Sum of first 5 numbers divisible by 3 and 5 is " + sum);
+    }
+}
