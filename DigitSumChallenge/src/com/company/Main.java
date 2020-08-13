dd14eba4eefa78e9e81fc1276283a9da5f560d7d
Robin Tram <robin.tram@gmail.com>
2020-08-13 10:02:43 -0400

    Number To Words Challenge

--- DigitSumChallenge/src/com/company/Main.java
+++ DigitSumChallenge/src/com/company/Main.java
@@ -19,4 +19,18 @@ public class Main {
 
         return sum;
     }
+
+    public static void printFactors(int number) {
+        if (number < 1) {
+            System.out.println("Invalid Value");
+        }
+
+        for (int i = 0; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
 }
+
