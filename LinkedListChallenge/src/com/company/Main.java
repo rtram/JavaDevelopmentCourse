4c7a123f0f65c9461ec10a69f209a3b03bd51bbe
Robin Tram <robin.tram@gmail.com>
2020-09-10 16:32:49 -0400

    Linked List Challenge: Create Seed Data

--- LinkedListChallenge/src/com/company/Main.java
+++ LinkedListChallenge/src/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Spotify spotify = new Spotify();
+        seedData(spotify);
+
         Scanner scanner = new Scanner(System.in);
         boolean quit = false;
 
@@ -18,6 +22,7 @@ public class Main {
                     quit = true;
                     System.out.println("Goodbye!");
                 case 1:
+                    albumsMenu(scanner, spotify);
 
             }
         }
@@ -33,19 +38,43 @@ public class Main {
         System.out.println("----------------------------");
     }
 
-    public static void albumsMenu(Scanner scanner) {
+    public static void albumsMenu(Scanner scanner, Spotify spotify) {
         boolean quit = false;
 
         while(!quit) {
-
+            printAlbumsMenu();
         }
     }
 
     public static void printAlbumsMenu() {
-
+        System.out.println("Albums Menu");
+        System.out.println("");
     }
 
     public static void printPlaylistMenu() {
 
+    }
+
+    public static boolean seedData(Spotify spotify) {
+        Album detroit2 = new Album("Detroit 2");
+        Song whywouldistop = new Song("Why Would I Stop?", "2:32");
+        Song luckyMe = new Song("Lucky Me", "4:08");
+        Song wolves = new Song("Wolves", "3:20");
+
+        detroit2.addSong(whywouldistop);
+        detroit2.addSong(luckyMe);
+        detroit2.addSong(wolves);
+
+        Album apromisemade = new Album("A Promise Made");
+        Song oath = new Song("Oath", "2:27");
+        Song virtualFriends = new Song("Virtual Friends", "3:47");
+        Song casualTrouble = new Song("Casual Trouble", "3:12");
+
+        apromisemade.addSong(oath);
+        apromisemade.addSong(virtualFriends);
+        apromisemade.addSong(casualTrouble);
+
+        spotify.addAlbum(detroit2);
+        spotify.addAlbum(apromisemade);
     }
 }
